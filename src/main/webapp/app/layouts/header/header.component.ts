ef9aae550568b7326326c8e7254573c927a8c5fc
Victor Nunes dos Santos <44742@mpro.mp.br>
2024-03-20 14:08:20 -0400

    :lipstick: update header component template and add to main component

--- src/main/webapp/app/layouts/header/header.component.ts
+++ src/main/webapp/app/layouts/header/header.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import FindLanguageFromKeyPipe from '../../shared/language/find-language-from-key.pipe';
+import HasAnyAuthorityDirective from '../../shared/auth/has-any-authority.directive';
+import { NgbDropdown, NgbDropdownMenu, NgbDropdownToggle } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import TranslateDirective from '../../shared/language/translate.directive';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'jhi-header',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent,
+    FindLanguageFromKeyPipe,
+    HasAnyAuthorityDirective,
+    NgbDropdown,
+    NgbDropdownMenu,
+    NgbDropdownToggle,
+    RouterLink,
+    RouterLinkActive,
+    TranslateDirective,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public sidebarService: SidebarService) {}
+
+  toggleCollapse() {
+    this.sidebarService.updateIsCollapsed(!this.sidebarService.isCollapsed);
+  }
+}
