e1ba7580d8526f666bcc22c3d2bb3bde52898ae3
Victor Nunes dos Santos <44742@mpro.mp.br>
2024-03-20 09:46:06 -0400

    :zap: add function to show/hide card clicking sidebar buttons

--- src/main/webapp/app/layouts/card-sistema/card-sistema.component.ts
+++ src/main/webapp/app/layouts/card-sistema/card-sistema.component.ts
@@ -1,17 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { ISistema } from '../../entities/sistema/sistema.model';
 import { SistemaService } from '../../entities/sistema/service/sistema.service';
-import { NgOptimizedImage } from '@angular/common';
+import { NgIf, NgOptimizedImage } from '@angular/common';
+import { ToggleSistemaService } from '../../services/toggle-sistema.service';
 
 @Component({
   selector: 'jhi-card-sistema',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, NgIf],
   templateUrl: './card-sistema.component.html',
   styleUrl: './card-sistema.component.scss',
 })
 export class CardSistemaComponent {
-  constructor(protected sistemaService: SistemaService) {}
+  constructor(
+    protected sistemaService: SistemaService,
+    protected toggleSistemaService: ToggleSistemaService,
+  ) {}
   @Input() sistemas?: ISistema[];
   @Input() trackId = (_index: number, item: ISistema): number => this.sistemaService.getSistemaIdentifier(item);
+
+  getArea(id: number | undefined) {
+    if (id === 1001) {
+      return 'area-fim';
+    } else if (id === 1002) {
+      return 'area-administrativa';
+    } else {
+      return 'mais-acessados';
+    }
+  }
 }
