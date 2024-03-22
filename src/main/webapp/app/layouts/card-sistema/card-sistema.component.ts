084534b0dea6e7fb9d906ffb3b9e4fafdcd71896
Victor Nunes dos Santos <44742@mpro.mp.br>
2024-03-22 14:01:42 -0400

    work in progress

--- src/main/webapp/app/layouts/card-sistema/card-sistema.component.ts
+++ src/main/webapp/app/layouts/card-sistema/card-sistema.component.ts
@@ -3,6 +3,7 @@ import { ISistema } from '../../entities/sistema/sistema.model';
 import { SistemaService } from '../../entities/sistema/service/sistema.service';
 import { NgIf, NgOptimizedImage } from '@angular/common';
 import { ToggleSistemaService } from '../../services/toggle-sistema.service';
+import { LocalStorageService } from '../../local-storage.service';
 
 @Component({
   selector: 'jhi-card-sistema',
@@ -15,6 +16,7 @@ export class CardSistemaComponent {
   constructor(
     protected sistemaService: SistemaService,
     protected toggleSistemaService: ToggleSistemaService,
+    protected localStorageService: LocalStorageService,
   ) {}
   @Input() sistemas?: ISistema[];
   @Input() trackId = (_index: number, item: ISistema): number => this.sistemaService.getSistemaIdentifier(item);
@@ -28,4 +30,15 @@ export class CardSistemaComponent {
       return 'mais-acessados';
     }
   }
+
+  logSistema(key: number, sistema: string | null | undefined) {
+    const item = { key, sistema };
+    this.localStorageService.adicionarAoHistorico(item);
+    //this.localStorageService.setItem(key, sistema);
+  }
+  obterUltimosItens() {
+    const ultimosItens = this.localStorageService.recuperarUltimosItens();
+    return ultimosItens;
+  }
+  protected readonly JSON = JSON;
 }
